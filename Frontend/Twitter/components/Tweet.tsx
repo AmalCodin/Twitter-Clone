527ebb109a55c1c5d0f37f6e0af686a98e34552f
Amal Shaji <61158962+AmalCodin@users.noreply.github.com>
2024-04-19 10:49:44 +0530

    Made All Tweets Render Properly

--- Frontend/Twitter/components/Tweet.tsx
+++ Frontend/Twitter/components/Tweet.tsx
@@ -1,8 +1,14 @@
 
 import {View, Text, Image, StyleSheet} from 'react-native';
 
+import { TweetType } from '../types';
 
-const Tweet = ({ tweet }) => {
+
+type TweetProps = {
+  tweet: TweetType;
+};
+const Tweet = ({ tweet }: TweetProps) => {
+  console.log(tweet.user.image?.toLowerCase())
     
     return (
         <View style={styles.container}>
